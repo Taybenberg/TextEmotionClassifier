2ae786ba90d9cc28a0a560ce35b4e9d6b8f32650
Vitalii Vdovenko <vdovenko.vtl@gmail.com>
2022-11-20 23:18:55 +0200

    Updated sentiment frequency counter

--- TextPreprocessing/WordAppearanceCounter.cs
+++ TextPreprocessing/WordAppearanceCounter.cs
@@ -1,30 +1,29 @@
-﻿namespace TextPreprocessing
+﻿using System.Text.Json.Serialization;
+
+namespace TextPreprocessing
 {
     public class WordAppearanceCounter
     {
         private static readonly int _sentimentsLength = Enum.GetNames(typeof(Sentiment)).Length;
 
         public int[] SentimentCounter { get; set; } = new int[_sentimentsLength];
-        public int TotalFrequency { get; set; } = 0;
-        public int DocumentFrequency { get; set; } = 0;
+        public int TotalFrequency { get; set; }
+        public int DocumentFrequency { get; set; }
 
-        private double[] _sentimentFrequecy;
+        [JsonIgnore]
         public double[] SentimentFrequency
         {
             get
             {
-                if (_sentimentFrequecy is null)
-                {
-                    _sentimentFrequecy = new double[_sentimentsLength];
+                var sentimentFrequency = new double[_sentimentsLength];
 
-                    if (TotalFrequency > 0)
-                    {
-                        for (int i = 0; i < _sentimentsLength; i++)
-                            _sentimentFrequecy[i] = SentimentCounter[i] / (double)TotalFrequency;
-                    }
+                if (TotalFrequency > 0)
+                {
+                    for (int i = 0; i < _sentimentsLength; i++)
+                        sentimentFrequency[i] = SentimentCounter[i] / (double)TotalFrequency;
                 }
 
-                return _sentimentFrequecy;
+                return sentimentFrequency;
             }
         }
     }
